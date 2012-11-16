ee46abb8a668701b9554ff76bd39afca828c21b8
Paul Batum <paul.batum@gmail.com>
2012-11-16 14:34:19 -0800

    Added a tables file

--- samples/alinefour/scripts/games.insert.js
+++ samples/alinefour/scripts/games.insert.js
@@ -1,6 +1,6 @@
 function insert(game, user, request) {
-    if(user.userId)
-        game.player1 = user.userId;
+
+    game.player1 = user.userId;
     game.state = JSON.stringify(newBoard());
     game.activePlayer = Math.floor((Math.random()*2)+1);
     request.execute();
