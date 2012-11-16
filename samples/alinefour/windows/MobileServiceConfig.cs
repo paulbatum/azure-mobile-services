5b77779d98127370e2740285053479a04f2b7e3b
Paul Batum <paul.batum@gmail.com>
2012-11-15 16:26:32 -0800

    Created win8 project from Mobile Services quickstart

---
+++ samples/alinefour/windows/MobileServiceConfig.cs
@@ -0,0 +1,15 @@
+﻿namespace alinefour
+{
+    public class MobileServiceConfig
+    {
+        public static string ApplicationKey 
+        {
+            get { return "";  }
+        }    
+
+        public static string ApplicationUri
+        {
+            get { return ""; }
+        }
+    }
+}
