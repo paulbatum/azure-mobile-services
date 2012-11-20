1900c3cfd36f4fc88d3f6c5d2a0b49b417dc8de3
Yavor Georgiev <yavorg@microsoft.com>
2012-11-20 17:57:01 +0100

    Fixing game creation, which I inadvertently broke

--- samples/alinefour/scripts/games.insert.js
+++ samples/alinefour/scripts/games.insert.js
@@ -2,19 +2,21 @@ function insert(game, user, request) {
 
     game.player1 = user.userId;
     game.state = JSON.stringify(newBoard());
-    game.activePlayer = Math.floor((Math.random()*2)+1);
+    game.activePlayer = Math.floor((Math.random() * 2) + 1);
+    delete game.player1Nickname;
+    delete game.player2Nickname;
     request.execute();
 
 }
 
 function newBoard() {
-	return [
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0],
-		[0,0,0,0,0,0]
-	];
+    return [
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0],
+		[0, 0, 0, 0, 0, 0]
+    ];
 }
